db89f219f74793ace8019323496e59f34baeeae1
Kelvinswr <Kelvinswr99@gmail.com>
2019-01-05 20:46:10 +0700

    Login and register activity

---
+++ src/main/java/com/lokabi/login_register_lokabi/loginActivity.java
@@ -0,0 +1,54 @@
+package com.lokabi.login_register_lokabi;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class loginActivity extends AppCompatActivity {
+
+    private TextView textView;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        textView = findViewById(R.id.tv_register);
+        RelativeLayout relativeLayout = findViewById(R.id.login_layout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(2000);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginActivity.this, registerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (animationDrawable != null && animationDrawable.isRunning()){
+            animationDrawable.stop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (animationDrawable != null && !animationDrawable.isRunning()){
+            animationDrawable.start();
+        }
+    }
+}
---
+++ src/main/java/com/lokabi/login_register_lokabi/registerActivity.java
@@ -0,0 +1,36 @@
+package com.lokabi.login_register_lokabi;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class registerActivity extends AppCompatActivity {
+    AnimationDrawable animationDrawable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        RelativeLayout relativeLayout = findViewById(R.id.register_layout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(2000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (animationDrawable != null && animationDrawable.isRunning()){
+            animationDrawable.stop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (animationDrawable != null && !animationDrawable.isRunning()){
+            animationDrawable.start();
+        }
+    }
+}
